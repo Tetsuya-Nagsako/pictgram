b0fc973c4f90cabb9a3c522dd69f65faa8f3eb43
tetsuya <nunobokono@gmail.com>
2021-01-05 08:44:18 +0000

    kadai04

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 1, maximum: 15}
+  
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
 
+  VARID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+  
+  has_secure_password
 end

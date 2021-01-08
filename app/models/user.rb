b54ab60bb7f92d7d916b76e97f8fb04c725f3546
tetsuya <nunobokono@gmail.com>
2021-01-08 06:13:43 +0000

    save0108

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
 
-  VALID_PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX, message:"は半角8~32文字英文字・数字それぞれ１文字以上含む必要があります"}
 
   has_secure_password
